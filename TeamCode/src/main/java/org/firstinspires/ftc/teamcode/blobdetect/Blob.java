e687a82ac4ce08e1c643027959009240a577c127
Joshua Bromley <joshua@mattbromley.com>
2019-10-14 19:00:11 -0700

    Finished blobs (untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blobdetect/Blob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blobdetect/Blob.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.blobdetect;
 
+import static java.lang.Math.abs;
+
 public class Blob {
     static int id;
     int x, y;
@@ -24,6 +26,43 @@ public class Blob {
         this.color = color;
     }
 
+    public void concattenate(Blob newBlob){
+        this.height = abs((newBlob.y + newBlob.height) - this.y);
+        this.width = abs((newBlob.x + newBlob.width)- this.x);
+        this.x = this.x < newBlob.x ? this.x : newBlob.x;
+        this.y = this.y < newBlob.y ? this.y : newBlob.y;
+        newBlob.id = -1;
+
+
+    }
+
+    public void addpixel(){
+        this.width++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getColor() {
+        return color;
+    }
 
 
 }
